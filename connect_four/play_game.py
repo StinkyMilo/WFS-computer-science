4657edb116a4d2757c63273c561e4e5ee9da1b8b
StinkyMilo <computercow.games@gmail.com>
2020-08-16 15:16:06 -0400

    play_game.py can now conduct a tournament and output the results as a dictionary.

--- connect_four/play_game.py
+++ connect_four/play_game.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from time import sleep
 import game_logic as game
 import ai
+import json
 GRAVITY = 0.01
 pygame.init()
 pygame.font.init()
@@ -82,6 +83,61 @@ def play_pygame_ai(p1ai, p2ai,fps=500):
                     elif turn==2:
                         turn=1
                         pending_move = p1.make_move(board)
+                    if pending_move != -1 and not game.is_valid_move(board,pending_move):
+                        print(pending_move)
+                        winner = 2 if turn == 1 else 1
+                    move_completed=False
+
+
+def replay_pygame(moves,fps=500):
+    pygame.display.set_caption("Connect 4")
+    screen = pygame.display.set_mode((400,400))
+    running = True
+    board = game.initialize_board()
+    move_num = 0
+    pending_move = moves[move_num]
+    move_completed = False
+    turn = 1
+    time = 0
+    winner = -1
+    while running:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        if winner != -1:
+            if winner==1:
+                text = "Red Wins"
+            elif winner==2:
+                text = "Black Wins"
+            else:
+                text = "It's a Tie"
+            text = font.render(text,True,(100,150,150))
+            text_width = text.get_rect().width
+            draw_board(screen, board)
+            screen.blit(text,(int(200-text_width/2),150))
+            pygame.display.update()
+        else:
+            move_completed = animate_move(screen, board, pending_move, turn, time)
+            time += 1
+            pygame.display.flip()
+            if move_completed:
+                board, _ = game.make_move(board, pending_move, turn)
+                end_check = game.check_game_end(board)
+                if end_check!=-1:
+                    winner = end_check
+                else:
+                    time=0
+                    if turn==1:
+                        turn=2
+                    elif turn==2:
+                        turn=1
+                    move_num+=1
+                    if move_num>=len(moves):
+                        winner = 0
+                    else:
+                        pending_move = moves[move_num]
                     if pending_move != -1 and not game.is_valid_move(board,pending_move):
                         print(pending_move)
                         winner = 2 if turn == 1 else 1
@@ -246,7 +302,9 @@ def play_print(p1ai,p2ai,pause_after_p1=False,pause_after_p2=False):
     game.print_board(board)
     return game_end
 
+
 def play_normal(p1ai,p2ai,print_winner=False):
+    moves = []
     p1 = p1ai(game.RED)
     p2 = p2ai(game.BLACK)
     turn = 0
@@ -258,12 +316,14 @@ def play_normal(p1ai,p2ai,print_winner=False):
             if move < 0 or move > 6 or game.get_y_of(board, move) == -1:
                 game_end = game.BLACK
                 break
+            moves.append(move)
             board, _ = game.make_move(board, move, game.RED)
         else:
             move = p2.make_move(board)
             if move < 0 or move > 6 or game.get_y_of(board, move) == -1:
                 game_end = game.RED
                 break
+            moves.append(move)
             board, _ = game.make_move(board, move, game.BLACK)
         turn += 1
         game_end = game.check_game_end(board)
@@ -274,21 +334,68 @@ def play_normal(p1ai,p2ai,print_winner=False):
             print("Black Wins")
         else:
             print("It's a tie.")
-    return game_end
-# idea: Check if AI is deterministic. If both are deterministic, simulate only 1 game. Otherwise simulate a bunch and see which wins most.
+    return game_end, moves
 
 
 def simulate_matches(ai_1,ai_2,match_count=1):
     wins_1 = 0
     wins_2 = 0
     ties = 0
+    games = []
     for _ in range(0,match_count):
-        winner = play_normal(ai_1,ai_2)
+        winner, moves = play_normal(ai_1,ai_2)
+        games.append({"moves":moves,"winner":winner})
         if winner==game.RED:
             wins_1+=1
         elif winner==game.BLACK:
             wins_2+=1
         elif winner==game.EMPTY:
             ties+=1
-    return wins_1, wins_2, ties
+    return wins_1, wins_2, ties, games
 
+# Code is made, figure out tournament
+def do_tournament(ai_list,match_count=20):
+    # All AI's play each other, return dictionary of statistics. Other functions will display the statistics
+    game_stats = {}
+    for ai in ai_list:
+        game_stats[ai.name] = {"matches":{}}
+        wins = 0
+        losses = 0
+        ties = 0
+        games_counted=0
+        games_played=0
+        matches_won = 0
+        matches_played = 0
+        for opponent in ai_list:
+            results = simulate_matches(ai,opponent,match_count)
+            game_stats[ai.name]["matches"][opponent.name] = {"results":(0,0,0),"replays":[]}
+            game_stats[ai.name]["matches"][opponent.name]["results"] = results[0:3]
+            game_stats[ai.name]["matches"][opponent.name]["replays"] = results[3]
+            # AI plays itself and the results are recorded but don't count toward overall statistics.
+            if ai != opponent:
+                games_counted+=match_count-results[2]
+                games_played+=match_count
+                wins+=results[0]
+                losses+=results[1]
+                ties+=results[2]
+                matches_played+=1
+                if results[0]>results[1]:
+                    matches_won+=1
+        game_stats[ai.name]["stats"] = {"raw_wins":wins,"raw_losses":losses,"raw_ties":ties,"games_played":games_played,"raw_win_rate":float(wins)/games_counted,"matches_won":matches_won,"adjusted_win_rate":float(matches_won)/matches_played}
+    return game_stats
+
+
+def display_tournament_results(results):
+    # Results is result of do_tournament function
+    # Page 1:
+    # Shows a grid of which ones won, hovering shows more specific stats
+    # Clicking will display a new game with the AI's playing each other.
+    # Page 2:
+    # Shows each AI's stats individually. Clicking the AI will start a human game against it.
+    pass
+
+
+# ais_playing = [game.ai_classes[1],game.ai_classes[2]]
+# open('output.json','w').write(json.dumps(do_tournament(ais_playing)))
+# game_json = json.loads(open('output.json','r').read())
+# replay_pygame(game_json["Random"]["matches"]["Random"]["replays"][0]["moves"])
